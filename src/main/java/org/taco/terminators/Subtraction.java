8615fe4cb54f44a43145142e1844a63e703e1ee3
Premkumar Manivannan <premgallant@gmail.com>
2024-04-14 09:48:24 -0400

    adding subtraction to calculator

--- src/main/java/org/taco/terminators/Subtraction.java
+++ src/main/java/org/taco/terminators/Subtraction.java
@@ -1,2 +1,8 @@
-package org.taco.terminators;public class Subtraction {
+package org.taco.terminators;
+
+public class Subtraction {
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
 }
