11f817b00fa63de7906505f7fbdcbf0dc2aafed2
“Prem” <premgallant@gmail.com>
2024-04-21 08:55:45 -0400

    implemented cuberoot

---
+++ src/main/java/org/taco/terminators/CubeRoot.java
@@ -0,0 +1,8 @@
+public class CubeRoot {
+
+  public static void main(String[] args) {
+    double number = 27;
+    double cubeRoot = Math.cbrt(number);
+    System.out.println("The cube root of " + number + " is " + cubeRoot);
+  }
+}
