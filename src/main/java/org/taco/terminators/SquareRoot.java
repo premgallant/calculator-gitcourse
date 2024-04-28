0231164c0d89288a61dfdfdd2f61561e11581cfb
Premkumar Manivannan <premgallant@gmail.com>
2024-04-28 10:09:57 -0400

    my custom square root

--- src/main/java/org/taco/terminators/SquareRoot.java
+++ src/main/java/org/taco/terminators/SquareRoot.java
@@ -1,6 +1,15 @@
 
 public class SquareRoot{
 
+   public static double squareRoot(double number) {
+        double guess = 1.0;
+        while (Math.abs(guess * guess - number) > 0.001) {
+            guess = (guess + number / guess) / 2.0;
+        }
+        return guess;
+    }
+
+
   public int computesquareroot(int num)
 {
    return num*num;
@@ -9,6 +18,8 @@ public class SquareRoot{
    SquareRoot sq = new SquareRoot();
    int result = sq.computesquareroot(10);
    System.out.println(result);
+  double resultFromCustomSquareRoot = squareRoot(25);
+  System.out.println(resultFromCustomSquareRoot);
 }
 
 }
