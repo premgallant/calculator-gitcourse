e3b7b5e5c726a99b345491ea03aeab5ada162b79
CamilleKanofsky <camille@rsdiv.com>
2024-04-28 13:21:32 -0400

    camille sqaure root file

---
+++ src/main/java/org/taco/terminators/SquareCamille.java
@@ -0,0 +1,19 @@
+package org.taco.terminators;
+
+public class SquareCamille {
+
+
+
+        public double computesquareroot(double num) {
+            return Math.sqrt(num);
+        }
+
+        public static void main(String args[]){
+            SquareCamille team1SquareRoot  = new SquareCamille();
+            double result = team1SquareRoot.computesquareroot(25);
+            System.out.println("Square root of 25 is\t"+result);
+        //GitTacoTeam1SquareRoot
+        }
+
+
+}
