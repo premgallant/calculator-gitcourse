00e15c8aad4d65d3802a9bb50f33f180384c0fc6
CamilleKanofsky <camille@rsdiv.com>
2024-05-27 18:28:46 -0400

    Update Calculator-Team-Squareroot.java

--- src/main/java/org/taco/terminators/Calculator-Team-Squareroot.java
+++ src/main/java/org/taco/terminators/Calculator-Team-Squareroot.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //public class TeamSquareRoot {
 //
 //   public double computesquareroot(double num) {
@@ -12,3 +13,19 @@
 //    }
 //
 //}
+=======
+class TeamSquareRoot {
+
+   public double computesquareroot(double num) {
+        return Math.sqrt(num);
+    }
+
+    public static void main(String args[]){
+        TeamSquareRoot team1SquareRoot  = new TeamSquareRoot();
+        double result = team1SquareRoot.computesquareroot(25);
+        System.out.println("Square root of 25 is\t"+result);
+
+    }
+
+}
+>>>>>>> Stashed changes
